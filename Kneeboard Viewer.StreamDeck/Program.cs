c6a6c1a70196df12d693d1a683c2b26e08e526b0
David Klasinc <david.klasinc@gmail.com>
2026-05-21 21:01:45 +0200

    Scaffold Stream Deck plugin project

---
+++ Kneeboard Viewer.StreamDeck/Program.cs
@@ -0,0 +1,25 @@
+// Kneeboard Viewer - a viewer for DCS World multiplayer mission kneeboards.
+// Copyright (C) 2026 David Klasinc
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// See the LICENSE file in the project root for the full license text.
+
+using BarRaider.SdTools;
+
+namespace KneeboardViewer.StreamDeck;
+
+public static class Program
+{
+    public static void Main(string[] args) => SDWrapper.Run(args, (IUpdateHandler?)null);
+}
